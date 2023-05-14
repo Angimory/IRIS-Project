e3d771cf89729891d7ee875ce545c5e95925c48a
Angimory <tonychung1707@gmail.com>
2023-05-14 13:29:40 -0700

    Added possible requests from users
    Added if statements that gives a response to possible requests made by users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,94 @@
 import './App.css';
 
-
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
-
-const completion = await openai.createChatCompletion({
-  model: "gpt-3.5-turbo",
-  messages: [{role: "user", content: "Hello world"}],
-});
-
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition()
+
+recognition.continuous = true;
+
+recognition.onresult = async function(event){
+  let current = event.resultIndex;
+  let transcript = event.results[current][0].transcript;
+  transcript = transcript.toLowerCase();
+  console.log(transcript);
+  speakThis(transcript);
+
+};
+
+function speakThis(message) {
+  const speech = new SpeechSynthesisUtterance();
+
+  speech.text = "I did not understand what you said please try again";
+
+  if(message.includes('hey') || message.includes('hello')) {
+      const finalText = "Hello sir";
+      speech.text = finalText;
+  }
+
+  else if(message.includes('how are you')) {
+      const finalText = "I am fine sir tell me how can i help you";
+      speech.text = finalText;
+  }
+
+  else if(message.includes('name')) {
+      const finalText = "My name is Iris";
+      speech.text = finalText;
+  }
+
+  else if(message.includes('open google')) {
+      window.open("https://google.com", "_blank");
+      const finalText = "Opening Google";
+      speech.text = finalText;
+  }
+
+  else if(message.includes('open instagram')) {
+      window.open("https://instagram.com", "_blank");
+      const finalText = "Opening instagram";
+      speech.text = finalText;
+  }
+
+  else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+      window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+      const finalText = "This is what i found on internet regarding " + message;
+      speech.text = finalText
+  }
+
+  else if(message.includes('wikipedia')) {
+      window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+      const finalText = "This is what i found on wikipedia regarding " + message;
+      speech.text = finalText;
+  }
+
+  else if(message.includes('time')) {
+      const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+      const finalText = time;
+      speech.text = finalText;
+  }
+
+  else if(message.includes('date')) {
+      const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+      const finalText = date;
+      speech.text = finalText;
+  }
+
+  else if(message.includes('calculator')) {
+      window.open('Calculator:///')
+      const finalText = "Opening Calculator";
+      speech.text = finalText;
+  }
+
+  else {
+      window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+      const finalText = "I found some information for " + message + " on google";
+      speech.text = finalText;
+  }
+
+  speech.volume = 1;
+  speech.pitch = 1;
+  speech.rate = 1;
+
+  window.speechSynthesis.speak(speech);
+}
+
 
 function startRecognition(){
   console.log("Active");
@@ -27,25 +100,7 @@ function endRecognition(){
   recognition.stop();
 };
 
-recognition.continuous = true;
 
-function readOut(message){
-  const speech = new SpeechSynthesisUtterance();
-  speech.text = message;
-  speech.volume = 1;
-  console.log(speech);
-  window.speechSynthesis.speak(speech);
-}
-
-recognition.onresult = async function(event){
-  let current = event.resultIndex;
-  let transcript = event.results[current][0].transcript;
-  console.log(transcript);
-  if (transcript.includes("open YouTube")){
-    window.open("https://www.youtube.com/");
-    readOut("Opening YouTube");
-  };
-};
 
 
 function App() {
@@ -55,8 +110,6 @@ function App() {
       <noscript>You need java script</noscript>
       <button onClick={startRecognition}>Start</button>
       <button onClick={endRecognition}>Stop</button>
-      <button onClick={e=>readOut("hello")}>Speak</button>
-      console.log(completion.data.choices[0].message);
     </div>
   );
 }
