d1deb556ea8a2bf7d6f9efdb9eaac46a0752aae4
Angimory <tonychung1707@gmail.com>
2023-05-31 12:27:08 -0700

    Response is connected

--- src/Response.js
+++ src/Response.js
@@ -1,40 +1,37 @@
 import axios from "axios";
-import { useState } from "react";
-import './App.css';
+import { useState, useEffect } from "react";
 
-function Response(){
-    const [prompt, setPrompt] = useState("");
-    const [response, setResponse] = useState("");
+export function Response(props) {
+  const [responseFromAi, setResponseFromAi] = useState("");
+  const speech = new SpeechSynthesisUtterance();
+  const { prompt } = props;
     
-   
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Send a request to the server with the prompt
-        axios
-          .post("http://localhost:8080/chat", { prompt })
-          .then((res) => {
-            // Update the response state with the server's response
-            setResponse(res.data);
-            console.log(response);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      };
-    
-      return (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p style={{color:"white"}}>{response}</p>
-        </div>
-      );
+  useEffect(() => {
+    if (prompt !== "") {
+      axios
+        .post("http://localhost:8080/chat", { prompt })
+        .then((res) => {
+          setResponseFromAi(res.data);
+          console.log(res.data);
+          console.log(prompt);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [prompt]);
+
+  useEffect(() => {
+    if (responseFromAi.length > 0) {
+      speech.text = responseFromAi;
+      speech.volume = 1;
+      speech.pitch = 1;
+      speech.rate = 1;
+      window.speechSynthesis.speak(speech);
+      setResponseFromAi("");
+    }
+  }, [responseFromAi]);
+
+  return null; // or render any desired UI components
 }
 
-export default Response;
