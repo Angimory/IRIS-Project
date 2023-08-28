ea1b19cd3fc7c6e7b42c48bfbc1886bc3947a5ec
Angimory <tonychung1707@gmail.com>
2023-08-28 12:03:20 -0700

    user can see that an error has happened

--- src/components/Response.js
+++ src/components/Response.js
@@ -5,20 +5,33 @@ export function Response(props) {
   const [responseFromAi, setResponseFromAi] = useState("");
   const speech = new SpeechSynthesisUtterance();
   const { prompt } = props;
-    
+  const [isVisible, setIsVisible] = useState(false);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    if (prompt !== "") {
-      axios
-        .post("http://localhost:8080/chat", { prompt })
-        .then((res) => {
+    let isMounted = true;
+
+    if (prompt !== '') {
+      const request = axios.post('http://localhost:8080/chat', { prompt });
+
+      request.then((res) => {
+        if (isMounted) {
           setResponseFromAi(res.data);
-          console.log(res.data);
-          console.log(prompt);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+          setError(''); // Clear any previous errors
+          setIsVisible(false); // Hide the error message
+        }
+      }).catch((err) => {
+        console.error(err);
+        if (isMounted) {
+          setError(err.message); // Use err.message to get the error message string
+          setIsVisible(true); // Show the error message
+        }
+      });
     }
+
+    return () => {
+      isMounted = false;
+    };
   }, [prompt]);
 
   useEffect(() => {
@@ -32,6 +45,21 @@ export function Response(props) {
     }
   }, [responseFromAi]);
 
-  return null; // or render any desired UI components
-}
+  useEffect(() => {
+    if (isVisible) {
+      const errorTimeout = setTimeout(() => {
+        setIsVisible(false); // Hide the error message after 3 seconds
+      }, 3000);
 
+      return () => {
+        clearTimeout(errorTimeout);
+      };
+    }
+  }, [isVisible]);
+
+  return (
+    <div>
+      {isVisible && <h2 className="error-text">{error}</h2>}
+    </div>
+  );
+}
